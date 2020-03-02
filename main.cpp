e8596fe282bef9e41ab75fc24d64c58dc2afd9e6
Sebastián Saldaña <A01570274@itesm.mx>
2020-03-02 11:04:36 -0600

    Added user input file functionality

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@
 using namespace std;
 
 int main(){
-    
+    cout << "Teclee el nombre del archivo que se debe analizar con la extension .txt " << endl;
+    string nombreArchEntrada;
+    getline(cin, nombreArchEntrada);
+    ifstream archEntrada;
+    archEntrada.open(nombreArchEntrada);
 return 0;
 }
