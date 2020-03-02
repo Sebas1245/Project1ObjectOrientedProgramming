674a8dedcc4e6fe37892a8b0af4630725a5e7e11
Sebastián Saldaña <A01570274@itesm.mx>
2020-03-02 11:20:14 -0600

    Added missing commands for file handling and a text file for testing

--- main.cpp
+++ main.cpp
@@ -8,8 +8,15 @@ int main(){
     cout << "Teclee el nombre del archivo que se debe analizar con la extension .txt " << endl;
     string nombreArchEntrada;
     getline(cin, nombreArchEntrada);
+    cout << "Ahora teclee el nombre del archivo de salida con la extension .txt" << endl;
+    string nombreArchSalida;
+    getline(cin,nombreArchSalida);
     ifstream archEntrada;
     archEntrada.open(nombreArchEntrada);
-    
+    ofstream archSalida;
+    archSalida.open(nombreArchSalida);
+   
+    archEntrada.close();
+    archSalida.close();
 return 0;
 }
