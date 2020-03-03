81c1ee6dcd6ae3d338ea53d1df1f2c7f0344785c
Sebastián Saldaña <A01570274@itesm.mx>
2020-03-03 13:14:13 -0600

    Added function for decoding operations and placing them in a string variable

--- main.cpp
+++ main.cpp
@@ -4,53 +4,59 @@
 
 using namespace std;
 
+// transformar
+// decodifica caracteres codificados en un arreglo para modificarlo en un string
+// Recibe: string operacion (donde esta guardada lo operacion aritmetica codificada), char arr[] (donde esta el "codigo")
+//Regresa: no regresa valores 
 void transformar(string operacion, char arr[]) {
     for (int i = 0; i < operacion.length(); i++){
         if (operacion[i] == arr[0]) {
-
+            operacion[i] = 0;
         }
         else if (operacion[i] == arr[1]) {
-
+            operacion[i] = 1;
         }
         else if (operacion[i] == arr[2]){
-        
+            operacion[i] = 2;
         }
         else if (operacion[i] == arr[3]) {
-
+            operacion[i] = 3;
         }
         else if (operacion[i] == arr[4]){
-        
+            operacion[i] = 4;
         }
         
         else if (operacion[i] == arr[5]) {
-
+            operacion[i] = 5;
         }
         else if (operacion[i] == arr[6]){
-        
+            operacion[i] = 6;
         }
         else if (operacion[i] == arr[7]) {
-
+            operacion[i] = 7;
         }
         else if (operacion[i] == arr[8]){
-        
+            operacion[i] = 8;
         }
-        
         else if (operacion[i] == arr[9]) {
-
+            operacion[i] = 9;
         }
         else if (operacion[i] == arr[10]){
-        
+            operacion[i] = '+';
         }
         
         else if (operacion[i] == arr[11]) {
-
+            operacion[i] = '-';
         }
         else if (operacion[i] == arr[12]){
-        
+            operacion[i] = '*';
         }
         
         else if (operacion[i] == arr[13]) {
-
+            operacion[i] = '/';
+        }
+        else {
+            operacion[i] = ' ';
         }
     }
 }
